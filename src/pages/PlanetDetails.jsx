b62dcdd586c5da98786d7ab5db5a9f6c10dd115c
flxemt <kudavidss@gmail.com>
2023-03-13 20:09:44 +0400

    minor fixes

--- src/pages/PlanetDetails.jsx
+++ src/pages/PlanetDetails.jsx
@@ -6,7 +6,11 @@ export default function Overview({ image, type }) {
   return (
     <>
       <div className="my-6 flex flex-col justify-center items-center md:col-span-2 xl:col-span-1 xl:row-span-2 xl:items-center">
-        <img className="max-w-[173px] md:max-w-[285px] xl:max-w-none" src={currentItem.images[image]} />
+        <img
+          className="max-w-[173px] md:max-w-[285px] xl:max-w-none"
+          src={currentItem.images[image]}
+          alt={currentItem.name}
+        />
         {type === 'geology' && (
           <img
             src={currentItem.images.geology}
@@ -17,7 +21,7 @@ export default function Overview({ image, type }) {
       </div>
       <div className="text-center md:text-left xl:max-w-[350px] xl:justify-self-end">
         <h1 className="font-display uppercase text-h2 leading-h2 xl:text-h1 xl:leading-h1">{currentItem.name}</h1>
-        <p className="mt-4 text-h4 leading-[2] xl:text-body xl:leading-body">{currentItem[type].content}</p>
+        <p className="mt-4 text-h4 leading-[2] md:mt-6 xl:text-body xl:leading-body">{currentItem[type].content}</p>
         <div className="flex justify-center md:justify-start items-center text-h3 leading-body mt-8 opacity-50 xl:mt-6 xl:text-body">
           <span className="mr-1">Source : </span>
           <a href={currentItem[type].source} target="_blank" className="font-bold underline">
